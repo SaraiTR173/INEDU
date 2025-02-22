42f5c658572576d92f094452310234a624a2f0ee
SaraiTR173 <sarai.tora010703@gmail.com>
2025-02-22 12:16:13 -0600

    new file: src/components/Login.tsx

---
+++ src/pages/Login.tsx
@@ -0,0 +1,21 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import ExploreContainer from '../components/Login';
+
+const Home: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Blank</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <ExploreContainer />
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;
