6d2d8d64a4faa7293a4bfe34b477046adc6d0b91
Aramiz <118640859+AramizBarSan@users.noreply.github.com>
2025-02-22 21:21:53 -0600

    Add files via upload

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,10 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import ExploreContainer from '../components/RegistroINEEA';
 
 const Home: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
